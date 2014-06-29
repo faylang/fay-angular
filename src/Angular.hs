b33e2a8c5e9e94d4ae458d5233b7e83843b2107d
Isaac Shapira <fresheyeball@gmail.com>
2014-06-28 21:45:17 -0600

    ctrl is now a seporate function

--- src/Angular.hs
+++ src/Angular.hs
@@ -12,5 +12,8 @@ newNgModule = ffi "angular.module(%1, %2)"
 ngModule :: String -> Fay NgModule
 ngModule = ffi "angular.module(%1)"
 
+ngCtrlPrep :: [String] -> (a -> Fay()) -> Fay [b]
+ngCtrlPrep = ffi "(function(){})()"
+
 ngController :: NgModule -> String -> (a -> Fay()) -> Fay NgController
 ngController = ffi "%1.controller(%2, ['$scope', %3])"
