278065e0f12549e3a59c4a218155304bbbafe9e9
Isaac Shapira <fresheyeball@gmail.com>
2014-06-28 21:43:02 -0600

    ctrl is now a seporate function

--- src/Main.hs
+++ src/Main.hs
@@ -6,9 +6,12 @@ import FFI
 setScopeStr :: NgScope -> String -> String -> Fay()
 setScopeStr = ffi "%1[%2] = %3"
 
+barCtrl :: NgScope -> Fay()
+barCtrl scope = do
+  setScopeStr scope "jello" "yellOw"
+
 main :: Fay()
 main = do
   foo   <- newNgModule "foo" []
-  bar   <- ngController foo "bar" $ \scope ->
-    setScopeStr scope "jello" "yellow"
+  bar   <- ngController foo "bar" barCtrl
   putStrLn "murf"
